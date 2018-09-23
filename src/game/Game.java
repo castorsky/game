54331ab740dcf67b26b8738bed0576e67fde2338
castor <castor@sedlex>
2018-09-23 12:19:52 +0800

    Some changes

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,7 +5,9 @@ import javax.swing.JOptionPane;
 public class Game {
 
     public static void main(String[] args) {
-    	String input = JOptionPane.showInputDialog(null, "Введите сложность игры (от 1 до 3): ", "Сложность игры", 1);
+    	String input = JOptionPane.showInputDialog(
+    			null, "Введите сложность игры (от 1 до 3): ", "Сложность игры", 1
+    			);
     	int difficulty = input.charAt(0)-'0';
     	if ((difficulty > 0) && (difficulty < 4)) {
 			Window window = new Window(difficulty);
